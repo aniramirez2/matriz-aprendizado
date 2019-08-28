8eca7b9b41b20238b713567d4a688c4ba76e0b4f
ana <anita-0621@hotmail.com>
2019-08-28 01:23:48 -0300

    alguns avanços nas perguntas e amostra de resultados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {visual, auditivo, cinestesico} from '../assets/texts'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aprendizado';
+  aprendizado="";
   vis =0;
   aud =0;
   cin =0;
@@ -16,6 +17,20 @@ export class AppComponent {
     {pergunta: 'lembro de pessoas com mais facilidade pelo', opcoes:[{'nome':'rosto', 'value':'vis'}, {'nome':'nome','value':'aud'}, {'nome':'momento ou local em que conheci', 'value':'cin'}]},
     {pergunta: 'Aprendo mais facilmente', opcoes:[{'nome':'lendo', 'value':'vis'}, {'nome':'ouvindo', 'value':'aud'}, {'nome':'fazendo', 'value':'cin'}]},
     {pergunta: 'As atividades que mais me motivam', opcoes:[{'nome':'fotografia', 'value':'vis'}, {'nome':'musica', 'value':'aud'}, {'nome':'dança', 'value':'cin'}]},
+    {pergunta: 'Na maioria das vezes, prefiro ', opcoes:[{'nome':'observar', 'value':'vis'}, {'nome':'ouvir', 'value':'aud'}, {'nome':'fazer', 'value':'cin'}]},
+    {pergunta: 'Ao lembrar um filme me vem a mente', opcoes:[{'nome':'algumas cenas', 'value':'vis'}, {'nome':'algumas falas', 'value':'aud'}, {'nome':'se gostei ou não do filme', 'value':'cin'}]},
+    {pergunta: 'Nas férias, gosto mais de', opcoes:[{'nome':'conhecer novos lugares', 'value':'vis'}, {'nome':'descansar', 'value':'aud'}, {'nome':'participar de atividades', 'value':'cin'}]},
+    {pergunta: 'O que mais valorizo nas pessoas é', opcoes:[{'nome':'aparencia', 'value':'vis'}, {'nome':'o que elas dizem', 'value':'aud'}, {'nome':'o que elas fazem', 'value':'cin'}]},
+    {pergunta: 'Percebo que alguém gosta de mim', opcoes:[{'nome':'pela expressao do rosto', 'value':'vis'}, {'nome':'pelo jeito que fala comigo', 'value':'aud'}, {'nome':'pelas atitudes que tem comigo', 'value':'cin'}]},
+    {pergunta: 'Meu carro preferido tem principalmente que ser', opcoes:[{'nome':'bonito', 'value':'vis'}, {'nome':'silencioso', 'value':'aud'}, {'nome':'confortable', 'value':'cin'}]},
+    {pergunta: 'Quando vou comprar algo, procuro', opcoes:[{'nome':'olhar o produto sem interferencia do vendedor', 'value':'vis'}, {'nome':'prefiro falar com um vendedor', 'value':'aud'}, {'nome':'testar e experimentar o produto', 'value':'cin'}]},
+    {pergunta: 'Tomo decisões com base principalmente', opcoes:[{'nome':'no que vejo', 'value':'vis'}, {'nome':'nas opinioes dos outros clientes', 'value':'aud'}, {'nome':'na minha vontade', 'value':'cin'}]},
+    {pergunta: 'Em excesso, o que mais me incomoda é', opcoes:[{'nome':'claridade', 'value':'vis'}, {'nome':'barulho', 'value':'aud'}, {'nome':'lugares cheios', 'value':'cin'}]},
+    {pergunta: 'O que mais me agrada num restaurante', opcoes:[{'nome':'ambiente', 'value':'vis'}, {'nome':'conversa', 'value':'aud'}, {'nome':'comida', 'value':'cin'}]},
+    {pergunta: 'Num show, valorizo mais a', opcoes:[{'nome':'iluminação e cenario', 'value':'vis'}, {'nome':'musicas', 'value':'aud'}, {'nome':'performance do cantor no palco', 'value':'cin'}]},
+    {pergunta: 'Enquanto espero alguém fico', opcoes:[{'nome':'observando o ambiente', 'value':'vis'}, {'nome':'ouvindo as conversas', 'value':'aud'}, {'nome':'andando', 'value':'cin'}]},
+    {pergunta: 'Eu mais me entusiasmo quando', opcoes:[{'nome':'me mostram', 'value':'vis'}, {'nome':'me contam', 'value':'aud'}, {'nome':'me convidam para participar', 'value':'cin'}]},
+    {pergunta: 'Ao consolar alguém, procuro', opcoes:[{'nome':'um caminho para a solução', 'value':'vis'}, {'nome':'uma palavra de conforto', 'value':'aud'}, {'nome':'um abraço', 'value':'cin'}]},
   ]
 
   addVote(resposta){
@@ -32,27 +47,29 @@ export class AppComponent {
       default:
         break;
     }
-    console.log("add vote added", resposta)
-    console.log('vis', this.vis)
-    console.log('aud', this.aud)
-    console.log('cin', this.cin)
   }
 
   seeResults(){
     var maxim= Math.max(this.vis, this.aud, this.cin);
-    switch (maxim) {
-      case this.vis:
-        alert('Você tem um aprendizaod visual')
-        break;
-      case this.aud:
-          alert('Você tem um aprendizaod auditivo')
+    console.log("primeiro", maxim)
+    if(maxim != 0){
+      switch (maxim) {
+        case this.vis:
+          this.aprendizado = visual
           break;
-      case this.aud:
-        alert('Você tem um aprendizaod cinestesico')
-        break;
-      default:
-        break;
-    } 
+        case this.aud:
+            this.aprendizado = auditivo
+            break;
+        case this.aud:
+          this.aprendizado = cinestesico
+          break;
+        default:
+          this.aprendizado= "ups não conseguimos calcular, tente de novo"
+          break;
+      } 
+    }else{
+      this.aprendizado = 'por favor preencha os campos'
+    }
   }
   
 }
---
+++ src/assets/texts.ts
@@ -0,0 +1,38 @@
+export var visual= `Características de uma pessoa visual:
+
+Pessoas com esse canal de aprendizagem normalmente criam imagens mentais, tem facilidade de memorizar imagens e rosto de pessoas, possuem uma memória fotográfica, tem mais interesses por filmes, séries, desenhos, e tem facilidade em memorizar assuntos quando acompanhado de cores.
+Pontos fortes do perfil visual.
+pessoas visuais tem uma visão 360 e notam coisas que normalmente passam despercebido pelas outras pessoas e se atentam aos detalhes
+Desafios do perfil visual.
+Tem facilidade de se distrair  com excesso de informação e poluição visual, e tem a tendência de viajar enquanto estão pensando.
+
+Qual a melhor forma de aprender sendo Visual?
+Pessoas que tem esse estilo de aprendizagem tendem a serem aquelas que sentam na frente da classe para terem uma melhor visão do professor e do que ele vier apresentar, e reparam nos gestos do professor e na imagem da sala naquele momento da explicação, adoram ver uma imagem durante apresentações, criam desenhos para se lembrarem e suas notas sempre vão ser as melhores da classe.
+Geralmente as pessoas que tem esse tipo de aprendizagem tem melhor aproveitamento educacional quando:
+⦁	Fazem um mapeamento do que aprendeu, para isso é uma boa dica usar o Mental Map, espécie de diagrama sistematizado contendo cores e ramificações com a explicação sobre determinado assunto
+⦁	Grifam o material que estão lendo ou já escreveram
+⦁	Criam listas de tópicos
+⦁	Usam diferentes cores para destacar alguma informação
+⦁	Podem visualizar o conteúdo de maneira mais ampla (como em um mapa mental)
+⦁	Fazem quadros, tabelas, desenhos e anotações sobre determinado assunto enquanto estuda
+⦁	Assistem videos e imagens que relacionem o que está aprendendo`;
+
+export var auditivo = `Esse canal de aprendizagem é representado por aqueles que gostam de falar, questionar, discutir.
+Para quem tem esse canal de aprendizagem fazer provas de escrita e leitura é uma tortura. Esse tipo de aprendiz costumam ler mais devagar e ficar repetindo o que leu mentalmente e pode ter um melhor rendimento se:
+⦁	Gravar aulas ou palestras para ouvir depois
+⦁	Criar rimas do que precisa se lembrar
+⦁	Estudar lendo em voz alta
+⦁	Explicar suas próprias idéias em um grupo de estudos
+⦁	Criar mnemônicos (frases fáceis de lembrar que explicam a matéria)
+⦁	Repetir oralmente aquilo que precisa aprender.`;
+
+export var cinestesico = `Para a turma que é Cinestésica o lema que os define é “Acho que vou fazer isso”.
+O estilo das pessoas que tem esse canal de aprendizagem pode ser definido por aqueles que gostam de fazer longas pausas em seus momento de estudos pois acham a teoria muito entediante e por isso preferem aplicar as teorias na prática.
+Pessoas cinestésicas tendem a ter um melhor aproveitamento dos estudos se:
+⦁	Puder realizar uma visitação em empresa
+⦁	Estudam em grupo
+⦁	Copiar a matéria da lousa
+⦁	Resolvem casos reais
+⦁	Usar jogos
+⦁	Criar materiais em que possa ser aplicada lógica
+Nem sempre alguém se encaixa 100% em apenas um, porém descobrir o perfil principal irá auxiliar na elaboração do seu plano de ação e definir a técnica de estudo adequada para o momento` 
